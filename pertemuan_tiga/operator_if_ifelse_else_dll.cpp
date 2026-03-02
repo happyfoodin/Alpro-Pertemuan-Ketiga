19bbfa8f2f8950ec2917469b5faf1d83caac7cd5
happyfoodin <hafifuddintales@gmail.com>
2026-03-02 09:23:22 +0700

    Add files via upload

---
+++ pertemuan_tiga/operator_if_ifelse_else_dll.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    system ("cls");
+    int x;
+
+    cout << "Masukkan nilai : " ;
+    cin >> x;
+    
+    // // if statement
+    // if (x > 75)
+    // {
+    //     cout << "anda lulus" << endl;
+    // }
+
+    // if else statement
+    if (x > 75) {
+        cout << "anda lulus" << endl;
+    } else {
+        cout << "anda tidak lulus" << endl;
+    }
+    
+
+
+}
